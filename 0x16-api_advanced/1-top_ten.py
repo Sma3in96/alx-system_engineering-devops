f784ceb24145935533fa2727f35d40ce25d7a58d
Sma3in96 <oumouss.ismail@gmail.com>
2024-04-10 14:06:46 +0000

    message

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -8,7 +8,7 @@ import requests
 def top_ten(subreddit):
     """method doc"""
     url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    headers = {"User-Agent": "Mozilla/1.0"}
+    headers = {'User-agent': 'python-requests/2.22.0'}
     response = requests.get(url, allow_redirects=False, headers=headers)
     if response.status_code == 200:
         data = response.json()
